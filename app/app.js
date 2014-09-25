06aabe6c7c25b89766066879f4251c9bf1217e6b
Lyubomir Mitkov <lyubomir.mitkov@gmail.com>
2014-09-26 01:22:41 +0300

    All Files For The Zip

---
+++ app/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('myApp', ['ngRoute'])
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+          templateUrl: 'app/views/home.html',
+          controller: 'homeController'
+      })
+      .when('/about', {
+          templateUrl: 'app/views/about.html',
+          controller: 'aboutController'
+      })
+      .otherwise({
+          redirectTo: '/'
+      });
+}])
+.controller('mainController', function ($scope) {
+    $scope.message = "Main Content";
+});;
+
